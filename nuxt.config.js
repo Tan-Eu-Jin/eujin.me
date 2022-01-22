07a2dc12e23a7af4f186e45abe4a8952f0d4cfce
Eu Jin Tan <eujin.tan@my.zalora.com>
2022-01-22 22:54:44 +0800

    updated font family of project

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700;900&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@100;400;700&display=swap",
       },
     ],
   },
