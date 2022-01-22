35e6fcf37a5a5a4455dc74d81267ca081c47d852
Eu Jin Tan <eujin.tan@my.zalora.com>
2022-01-22 22:55:39 +0800

    added title and meta attributes to the index file

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,18 @@
         <IndexGreeter />
     </v-app>
 </template>
+
+<script>
+export default {
+    head: {
+        title: "eujin.me 🚀",
+        meta: [
+            {
+                hid: "Website of Tan Eu Jin",
+                name: "Website of Tan Eu Jin",
+                content: "Website of Tan Eu Jin",
+            },
+        ],
+    },
+};
+</script>
